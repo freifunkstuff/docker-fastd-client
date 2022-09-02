5eff1ecbc43557541d792861eeaf64bb86011287
micw <michael@wyraz.de>
2022-09-02 20:55:27 +0200

    Fix healthcheck if only 1 peer is connected

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONNECTED_PEERS="$( socat - UNIX-CONNECT:/config/fastd/fastd.status | jq -r ".peers[] | select(.connection!=null) .name" )"
-CONNECTED_PEER_COUNT=$( echo -n "${CONNECTED_PEERS}" | wc -l)
+CONNECTED_PEER_COUNT=$( echo -n "${CONNECTED_PEERS}" | wc -w)
 
 if [ ${CONNECTED_PEER_COUNT} == 0 ]; then
   echo "No peers connected!"
